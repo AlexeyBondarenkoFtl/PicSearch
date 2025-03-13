73a649bea1a6dea14fdea4d45146e3530c8d39ee
alexeybondarenko <bondarenkoaa@sushivesla.team>
2025-03-13 16:09:54 +0300

    reworked navbar code

--- app/src/main/java/com/alexeybondarenko/picsearch/ui/main/NavigationItem.kt
+++ app/src/main/java/com/alexeybondarenko/picsearch/ui/main/NavigationItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.alexeybondarenko.picsearch.ui.utils.navigation.Routes
 
 // todo change icons
 // todo move text to res
@@ -15,22 +16,26 @@ enum class NavigationItem(
     val label: String,
     val selectIcon: ImageVector,
     val unselectIcon: ImageVector,
+    val associatedRoute: Routes,
 ) {
     SEARCH(
         "Поиск",
         Icons.Filled.Search,
-        Icons.Outlined.Search
+        Icons.Outlined.Search,
+        Routes.Search,
     ),
 
     SAVED(
         "Сохраненные",
         Icons.Filled.Favorite,
-        Icons.Outlined.FavoriteBorder
+        Icons.Outlined.FavoriteBorder,
+        Routes.Saved,
     ),
 
     SETTINGS(
         "Настройки",
         Icons.Filled.Settings,
-        Icons.Outlined.Settings
+        Icons.Outlined.Settings,
+        Routes.Settings,
     );
 }
